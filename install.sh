099e529e815e2cf08f90627ca1bc509de533de1b
Cal Henderson <cal@iamcal.com>
2018-04-16 22:19:12 +0000

    create symlinks to the 3 emoji projects and then ignore them

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 ln -s /var/www/html/unicodey.com/site.conf /etc/apache2/sites-available/unicodey.com.conf
 a2ensite unicodey.com
 service apache2 reload
+
+ln -s /home/ubuntu/emoji-data /var/www/html/unicodey.com/www/emoji-data
+ln -s /home/ubuntu/js-emoji /var/www/html/unicodey.com/www/js-emoji
+ln -s /home/ubuntu/php-emoji /var/www/html/unicodey.com/www/php-emoji
+
+
